0d447745c5f122bc0ee343464490e4453c85be3d
Balaji1597 <95161576+Balaji1597@users.noreply.github.com>
2021-11-28 13:50:15 +0530

    Add files via upload

---
+++ OddNumbers.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+
+	public class OddNumbers{
+		public static void main(String args[]){
+		int a[]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+		System.out.println("Odd Numbers:");
+		for(int i=0;i<a.length;i++){
+		if(a[i]%2!=0){
+		System.out.println(a[i]);
+		}
+		}
+		}
+
+		}
+
+
